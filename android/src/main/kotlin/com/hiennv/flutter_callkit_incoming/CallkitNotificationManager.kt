8d0f0fd0c71cd981c51b29c7cc2cab360facf786
Asteroid <ishimori11@gmail.com>
2025-02-26 18:35:22 +0900

    fix: set notificationBuilder.priority

--- android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitNotificationManager.kt
+++ android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitNotificationManager.kt
@@ -143,7 +143,7 @@ class CallkitNotificationManager(private val context: Context) {
         } catch (_: Exception) {
         }
         notificationBuilder.setChannelId(NOTIFICATION_CHANNEL_ID_INCOMING)
-        notificationBuilder.priority = NotificationCompat.PRIORITY_MAX
+        notificationBuilder.priority = NotificationManager.IMPORTANCE_HIGH
         val isCustomNotification =
             data.getBoolean(CallkitConstants.EXTRA_CALLKIT_IS_CUSTOM_NOTIFICATION, false)
         val isCustomSmallExNotification =
